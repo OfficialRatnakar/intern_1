20e80ee41f7c5f8977a79197c13d37c5cda18f29
Vishal Ratnakar <vishalratnakar453@gmail.com>
2025-06-20 16:50:58 +0530

    vite file updated

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,8 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [
     tailwindcss(),
-    react()
+    react(),
+
   ],
+  base: process.env.VITE_BASE_PATH || '/intern_1',
 })
